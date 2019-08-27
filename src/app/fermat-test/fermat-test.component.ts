a6d85937b0ae648e257f70810a4ed795fa4f65aa
timonbarth <timonnicholas.barth@gmail.com>
2019-08-27 18:21:48 +0200

    1 file modified

--- src/app/fermat-test/fermat-test.component.ts
+++ src/app/fermat-test/fermat-test.component.ts
@@ -12,24 +12,26 @@ export class FermatTestComponent implements OnInit {
 
   ergebnis = 'noch nicht berechnet';
 
- constructor() { }
+  constructor() { }
 
- fermattest() {
-   let zeuge = 2;
-   //Berechne zeuge^(versuchskaninchen-1) modulo versuchskaninchen
-   let zwischenergebnis = 1;
-   for (let i = 0; i < this.versuchskaninchen-1; i++) {
+  fermattest() {
+    this.ergebnis = 'Starte Fermat-Test der Zahl '
+                + this.versuchskaninchen + '... ';
+    let zeuge = 2;
+    //Berechne zeuge^(versuchskaninchen-1) modulo versuchskaninchen
+    let zwischenergebnis = 1;
+    for (let i = 0; i < this.versuchskaninchen - 1; i++) {
 
-   zwischenergebnis = zwischenergebnis * zeuge;
-   }
-  zwischenergebnis = zwischenergebnis % this.versuchskaninchen;
-  if (zwischenergebnis === 1)  {
-   this.ergebnis += 'Zeuge ' + zeuge +  ' OK... ';
-  } else {
-    this.ergebnis += 'Zeuge ' + zeuge + ' Nicht OK... ';
+      zwischenergebnis = zwischenergebnis * zeuge;
+    }
+    zwischenergebnis = zwischenergebnis % this.versuchskaninchen;
+    if (zwischenergebnis === 1) {
+      this.ergebnis += 'Zeuge ' + zeuge + ' OK... ';
+    } else {
+      this.ergebnis += 'Zeuge ' + zeuge + ' Nicht OK... ';
+    }
+
   }
-  this.ergebnis = 'Zwischenergebnis: ' + zwischenergebnis
- }
 
 
   ngOnInit() {
