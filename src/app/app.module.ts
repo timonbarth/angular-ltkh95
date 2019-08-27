d06e3a766d79fc808ecff7ca40c55eef90402122
timonbarth <timonnicholas.barth@gmail.com>
2019-08-27 16:52:55 +0200

    2 files modified, 2 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,11 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { SabwCaesarComponent } from './sabw-caesar/sabw-caesar.component';
+import { FermatTestComponent } from './fermat-test/fermat-test.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, SabwCaesarComponent ],
+  declarations: [ AppComponent, HelloComponent, SabwCaesarComponent, FermatTestComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
---
+++ src/app/fermat-test/fermat-test.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-fermat-test',
+  templateUrl: './fermat-test.component.html',
+  styleUrls: ['./fermat-test.component.css']
+})
+export class FermatTestComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
